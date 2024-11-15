c6f79dd394cdf905cb988c2bb9b90e18b3319302
abelcarriizo <abelcarriizo@outlook.com>
2024-11-15 14:28:15 -0300

    Ejemplos de principio de responsabilidad unica

---
+++ code/principios/SOLID/S. Principio de Responsabilidad Unica/bien.py
@@ -0,0 +1,7 @@
+class GestorArchivos:
+  def guardar(self):
+    print('Guardar archivo')
+
+class EnviarEmail:
+  def enviar(self):
+    print('Enviar email')
---
+++ code/principios/SOLID/S. Principio de Responsabilidad Unica/mal.py
@@ -0,0 +1,6 @@
+class GestorArchivos:
+  def guardar(self):
+    print('Archivo guardado')
+
+  def enviar_por_email(self):
+    print('Archivo enviado')
