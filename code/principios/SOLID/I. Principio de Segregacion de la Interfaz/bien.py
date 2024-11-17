35596bac38be9bb6829af388f77a954211068a8d
abelcarriizo <abelcarriizo@outlook.com>
2024-11-17 15:53:40 -0300

    Ejemplos de Principio de Segregacion de la Interfaz

---
+++ code/principios/SOLID/I. Principio de Segregacion de la Interfaz/bien.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+class CloudStorage(ABC):
+  @abstractmethod
+  def storeFile(self, name):
+    pass
+
+  @abstractmethod
+  def getFile(self, name):
+    pass
+
+class CloudHostingProvider(ABC):
+  @abstractmethod
+  def createServer(self, region):
+    pass
+
+  @abstractmethod
+  def listServers(self, region):
+    pass
+
+class Amazon(CloudStorage, CloudHostingProvider):
+  def storeFile(self, name):
+    print(f'Almacenando archivo: {name}')
+
+  def getFile(self, name):
+    print(f'Archivo obtenido: {name}')
+
+  def createServer(self, region):
+    print(f'Servidor creado en: {region}')
+
+  def listServers(self, region):
+    print(f'Lista de servidores en la region: {region}...')
+
+class Achicoria(CloudStorage):
+  def storeFile(self, name):
+    print(f'Almacenando archivo: {name}')
+
+  def getFile(self, name):
+    print(f'Archivo obtenido: {name}')
+  
