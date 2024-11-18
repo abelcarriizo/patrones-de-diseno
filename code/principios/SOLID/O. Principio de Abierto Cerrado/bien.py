9a3e409f33103b85621556781c088396df926c80
abelcarriizo <abelcarriizo@outlook.com>
2024-11-18 20:38:21 -0300

    O. Principio Abierto Cerrado actualizado

--- code/principios/SOLID/O. Principio de Abierto Cerrado/bien.py
+++ code/principios/SOLID/O. Principio de Abierto Cerrado/bien.py
@@ -1,18 +1,20 @@
-from abc import ABC, abstractmethod
+tasas_cambio = {
+    "USD_ARS": 20,    # 1 dólar equivale a 20 pesos
+    "ARS_USD": 0.05,  # 1 peso equivale a 0.05 dólares
+}
 
-class Operacion(ABC):
-  @abstractmethod
-  def calcular(self, a, b):
-    pass
+class Conversor():
+  def __init__(self, tasa_moneda):
+    self.tasas_cambio = tasa_moneda
 
-class Sumar(Operacion):
-  def calcular(self, a, b):
-    return a + b
-  
-class Restar(Operacion):
-  def calcular(self, a, b):
-    return a - b
-  
-class Calculadora:
-  def calcular(self, a, b, operacion:Operacion):
-    return operacion.calcular(a, b)
+  def convertir(self, cantidad, moneda_origen, moneda_destino):
+    clave = f"{moneda_origen}_{moneda_destino}"
+    tasa = self.tasas_cambio.get(clave)
+    if tasa is None:
+      raise ValueError(f"No existe una tasa de cambio para {clave}")
+    return cantidad * tasa
+
+if __name__=='__main__':
+  conversor = Conversor(tasas_cambio)
+  cantidad_en_peso = conversor.convertir(20, 'USD', 'ARS') # dolar a peso
+  print(f'20 USD son: "{cantidad_en_peso}ARS"')
--- code/principios/SOLID/O. Principio de Abierto Cerrado/mal.py
+++ code/principios/SOLID/O. Principio de Abierto Cerrado/mal.py
@@ -1,6 +1,20 @@
-class Calculadora:
-  def calcular(self, a:int, b:int, operacion:str):
-    if operacion == 'sumar':
-      return a + b
-    elif operacion == 'restar':
-      return a - b
+class ConversorMoneda:
+    def __init__(self):
+        self.tasa_dolar_a_peso = 350
+
+    def dolar_a_peso(self, dolares):
+        return dolares * self.tasa_dolar_a_peso
+
+    def peso_a_dolar(self, pesos):
+        return pesos / self.tasa_dolar_a_peso
+
+if __name__ == '__main__':
+    conversor = ConversorMoneda()
+    
+    dolares = 100
+    pesos = conversor.dolar_a_peso(dolares)
+    print(f'{dolares} dólares son {pesos} pesos argentinos.')
+    
+    pesos = 35000
+    dolares = conversor.peso_a_dolar(pesos)
+    print(f'{pesos} pesos argentinos son {dolares} dólares.')
